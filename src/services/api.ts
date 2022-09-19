8f6d542db13e3d6c6d6b4a4802304b41b2b04da5
Matheus <matheus.werneck@compasso.com.br>
2022-09-19 01:06:24 -0300

    chore: removed api key from header

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,4 @@ process.env.NEXT_PUBLIC_X_API_KEY;
 
 export const pokemontcgApi: Axios = axios.create({
   baseURL: "https://api.pokemontcg.io/v2",
-  headers: {
-    "X-Api-Key": "7b186217-ecfe-4956-9abb-d44a76ecad68",
-  },
 });
