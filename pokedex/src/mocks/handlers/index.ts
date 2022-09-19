2fed7ef680b05567f10053d874ca0dac24afc429
Matheus <matheus.werneck@compasso.com.br>
2022-09-18 21:14:50 -0300

    chore: changed folder organization

--- pokedex/src/mocks/handlers/index.ts
+++
@@ -1,8 +0,0 @@
-import { rest } from "msw";
-import { pokemonDataMock } from "../data/pokemonCardList";
-
-export const handlers = [
-  rest.get("https://api.pokemontcg.io/v2/cards", async (req, res, ctx) => {
-    return res(ctx.json(pokemonDataMock));
-  }),
-];
