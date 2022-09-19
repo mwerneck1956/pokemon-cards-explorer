7ea61c3b6948f3638b286e9ad57ad47062784985
Matheus <matheus.werneck@compasso.com.br>
2022-09-19 08:40:32 -0300

    env: added X-Api-Key header in axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios, { Axios } from "axios";
 
-process.env.NEXT_PUBLIC_X_API_KEY;
+const apikey = process.env.NEXT_PUBLIC_X_API_KEY || "";
 
 export const pokemontcgApi: Axios = axios.create({
   baseURL: "https://api.pokemontcg.io/v2",
+  headers: {
+    "X-Api-Key": apikey,
+  },
 });
